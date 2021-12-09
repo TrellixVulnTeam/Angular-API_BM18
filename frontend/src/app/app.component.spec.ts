bb40ee5e73986a85eb16867df678a85f5a5e53dc
Kiriiaq <78642576+Kiriiaq@users.noreply.github.com>
2021-12-09 11:01:13 +0100

    Test ready

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,5 +1,10 @@
 //App.component.spec.ts
 
+//Import des modules
+import { AppRoutingModule } from './app-routing.module';
+import { AuthInterceptor } from './_interceptors/auth-interceptor';
+
+
 //Import des composants
 import { AppComponent } from './app.component';
 import { StuffComponent } from './stuff/stuff.component';
@@ -16,8 +21,13 @@ import { ModifyUserComponent } from './user/modify-user/modify-user.component';
 import { SingleUserComponent } from './user/single-user/single-user.component';
 import { UserStuffListComponent } from './user/user-stuff-list/user-stuff-list.component';
 
-//
+//Import des modules angular
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 describe('AppComponent', () => { // 2
     beforeEach(async(() => { // 3
@@ -38,25 +48,35 @@ describe('AppComponent', () => { // 2
                 SingleUserComponent,
                 UserStuffListComponent,
             ],
-        }).compileComponents();
-    }));
+            imports: [
+                BrowserModule,
+                AppRoutingModule,
+                FormsModule,
+                ReactiveFormsModule,
+                BrowserAnimationsModule,
+                MatProgressSpinnerModule,
+                HttpClientModule
+            ],
+            providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+    }).compileComponents();
+}));
 
-    it('should create the app', () => { // 4
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
-    });
+it('should create the app', () => { // 4
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+});
 
-    it(`should have as title 'angular-component-testing'`, () => {  //5
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('angular-component-testing');
-    });
+it('should have as title \'angular-component-testing\'', () => {  //5
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+});
 
-    it('should render title in a h1 tag', () => { //6
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-component-testing!');
-    });
+it('should render title in a h1 tag', () => { //6
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-component-testing!');
+});
 });
